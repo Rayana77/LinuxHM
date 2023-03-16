c8a9d9b0b239bef3d64473d32c5fc97ebd31d7aa
Rayana77 <124196954+Rayana77@users.noreply.github.com>
2023-03-17 02:20:50 +0300

    Add files via upload

---
+++ fullscript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo"Hello Student!"
+echo -n "Enter your name: "
+read name echo "Welcome to terminal §name!"
+ 
+mkdir /tmp/test 
+touch /tmp/mydate.txt 
+echo "$(date + "%H:%M:%S") >/tmp/mydate.txt
+ 
+echo "Saving data..."
+ 
+for i in {10..1}; do 
+echo -n "$i " 
+sleep 0.5 
+done 
+echo "Data saved. 
+Continuing work..."
+ 
+df -h >> /tmp/mydate.txt mkdir /opt/mydate 
+cp /tmp/mydate.txt /opt/mydate/newmydate.txt
+ 
+echo "Waitng..." 
+for i in {5..1}; do 
+echo -n "$i " 
+sleep 1 
+done 
+echo "Well done Boss!" 
+"Well done Boss!"
+ 
